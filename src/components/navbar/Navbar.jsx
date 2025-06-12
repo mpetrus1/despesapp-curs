a905b614221e73f92ff68043206b0b9dfdb78ac1
Miquel <mpec001@gmail.com>
2025-06-12 15:55:18 +0200

    Navbar corregit i net

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { useEffect, useState } from "react";
 import './Navbar.css'
 import { auth, logoutUser, getParticipantAmbUid } from "../../firebase/firebase";
@@ -7,7 +7,7 @@ import { onAuthStateChanged } from "firebase/auth";
 export default function Navbar() {
 
   const [user, setUser] = useState(null);
-  const navigate = useNavigate();
+  
 
   const handleLogout = () => {
     logoutUser()
