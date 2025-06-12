de13c8094cd5e48434570a5a10b9beb87d8890a9
Miquel <101179847+mpetrus1@users.noreply.github.com>
2025-06-12 17:12:07 +0200

    Delete config

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-// Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyCn1Hz3TypEvHMNH8jiNoeJw-Ut6vTd4w4",
-  authDomain: "despesappmiquel.firebaseapp.com",
-  projectId: "despesappmiquel",
-  storageBucket: "despesappmiquel.firebasestorage.app",
-  messagingSenderId: "369311062473",
-  appId: "1:369311062473:web:757182b132c848105eb018"
-};
